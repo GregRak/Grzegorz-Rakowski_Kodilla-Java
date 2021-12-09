babcf216bb1b7c797312e5feabd6d7416ce676e5
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2021-12-09 19:03:43 +0100

    6.2 Added class Calculator and its test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args)
-    {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        Calculator calculator = new Calculator(20,12);
+
+        int addResult = calculator.addAToB();
+        int substractResult = calculator.subAFromB();
+
+        if (addResult==(calculator.getNumberA()+ calculator.getNumberB())) {
+            System.out.println("Calculator function: addition - OK");
+        } else {
+            System.out.println("Calculator function: addition - Error!");
+        }
+
+        if (substractResult==(calculator.getNumberA()- calculator.getNumberB())) {
+            System.out.println("Calculator function: substraction - OK");
+        } else {
+            System.out.println("Calculator function: substraction - Error!");
+        }
+
+
     }
 }
