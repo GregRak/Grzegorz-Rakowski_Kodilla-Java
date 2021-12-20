fd713fd329588ef9760f8a04cbdd601412f871a8
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2021-12-20 19:29:44 +0100

    7.3 Stream: Adding classes ForumUser and Forum + stream implementation

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,47 +1,28 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.Beautifier;
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        Forum forum = new Forum();
 
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10,5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10,5, (a, b) -> a / b);
+        Map<Integer, ForumUser> theMapOfForumUsers = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getDateOfBirth().isAfter(LocalDate.of(2001, 12, 20)))
+                .filter(forumUser -> forumUser.getPostsNumber() > 0)
+                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
 
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        System.out.println("\nTexts after use lambdas");
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("To jest pierwszy tekst do upiększenia",
-                (textToEmbellish) -> textToEmbellish.concat(" ABC"));
-        poemBeautifier.beautify("To jest drugi tekst do upiększenia",
-                (textToEmbellish) -> textToEmbellish.toUpperCase());
-        poemBeautifier.beautify("To jest trzeci tekst do upiększenia",
-                (textToEmbellish -> textToEmbellish.replace("e","E")));
-        poemBeautifier.beautify("To jest czwarty tekst do upiększenia",
-                textToEmbellish -> textToEmbellish.repeat(2));
-
-        System.out.println("\nTexts from class Beautifier");
-        poemBeautifier.beautify("To jest pierwszy tekst do upiększenia",
-                Beautifier::addABCInTheEnd);
-        poemBeautifier.beautify("To jest drugi tekst do upiększenia",
-                Beautifier::changeIntoUpperCase);
-        poemBeautifier.beautify("To jest trzeci tekst do upiększenia",
-                Beautifier::charsFrom0To10);
-        poemBeautifier.beautify("To jest czwarty tekst do upiększenia",
-                Beautifier::eReplaceIntoE);
+        System.out.println("We have " + theMapOfForumUsers.size() + " users with the given parameters");
+        theMapOfForumUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ", " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
