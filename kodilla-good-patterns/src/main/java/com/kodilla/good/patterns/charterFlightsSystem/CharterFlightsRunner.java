a35017b4531e4e7a8c13edc1db3695fcc9f2ac9e
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-02-13 14:59:18 +0100

    Module 13.5 CharterFlightSystem

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/charterFlightsSystem/CharterFlightsRunner.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/charterFlightsSystem/CharterFlightsRunner.java
@@ -3,6 +3,9 @@ package com.kodilla.good.patterns.charterFlightsSystem;
 public class CharterFlightsRunner {
 
     public static void main (String[] args) {
-
+        FlightSearcher flightSearcher = new FlightSearcher();
+   //     flightSearcher.findFlightsFromCity("Gdańsk");
+   //     flightSearcher.findFlightsToCity("Wrocław");
+        flightSearcher.findFlightsFromTo("Gdańsk", "Wrocław", true);
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/charterFlightsSystem/Flight.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/charterFlightsSystem/Flight.java
@@ -7,13 +7,13 @@ public class Flight {
     private final String departureCity;
     private LocalDateTime departureTime;
     private final String arrivalCity;
-    private LocalDateTime arrivalTima;
+    private LocalDateTime arrivalTime;
 
     public Flight(String departureCity, LocalDateTime departureTime, String arrivalCity, LocalDateTime arrivalTima) {
         this.departureCity = departureCity;
         this.departureTime = departureTime;
         this.arrivalCity = arrivalCity;
-        this.arrivalTima = arrivalTima;
+        this.arrivalTime = arrivalTima;
     }
 
     public String getDepartureCity() {
@@ -28,7 +28,39 @@ public class Flight {
         return arrivalCity;
     }
 
-    public LocalDateTime getArrivalTima() {
-        return arrivalTima;
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "departureCity='" + departureCity + '\'' +
+                ", departureTime=" + departureTime +
+                ", arrivalCity='" + arrivalCity + '\'' +
+                ", arrivalTime=" + arrivalTime +
+                "}\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Flight flight = (Flight) o;
+
+        if (!departureCity.equals(flight.departureCity)) return false;
+        if (!departureTime.equals(flight.departureTime)) return false;
+        if (!arrivalCity.equals(flight.arrivalCity)) return false;
+        return arrivalTime.equals(flight.arrivalTime);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = departureCity.hashCode();
+        result = 31 * result + departureTime.hashCode();
+        result = 31 * result + arrivalCity.hashCode();
+        result = 31 * result + arrivalTime.hashCode();
+        return result;
     }
 }
