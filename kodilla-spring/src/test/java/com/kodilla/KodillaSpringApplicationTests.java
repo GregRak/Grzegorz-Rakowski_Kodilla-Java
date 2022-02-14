1f37150013bade665404620b31cadf6d6986e9f4
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-02-14 20:34:58 +0100

    Module 14.2 Adnotation @Bean

--- kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -2,6 +2,7 @@ package com.kodilla;
 
 import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Square;
 import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,6 +34,17 @@ class KodillaSpringApplicationTests {
         String name = shape.getShapeName();
         //Then
         assertEquals("This is a triangle", name);
+    }
+
+    @Test
+    void testSquareLoadedIntoContainer() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Square.class);
+        //When
+        String result = shape.getShapeName();
+        //Then
+        assertEquals("This is a square.", result);
     }
 
     @Test
