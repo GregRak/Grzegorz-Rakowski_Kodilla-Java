116207575ff33e08352482ad4ed904901e059d9c
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-03-22 21:35:43 +0100

    Module 17.4 Hibernate: named queries - tasks

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -5,6 +5,12 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+        name = "Company.companiesWithNamesStartingWithFollowingCharacters",
+        query = "SELECT * FROM COMPANIES WHERE COMPANY_NAME LIKE CONCAT (:STARTING_CHARS, '%')",
+        resultClass = Company.class
+)
+
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -13,6 +15,8 @@ public class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany() {
@@ -22,7 +26,7 @@ public class CompanyDaoTestSuite {
         Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
 
         Company softwareMachine = new Company("Software Machine");
-        Company dataMasters = new Company("Data Maesters");
+        Company dataMasters = new Company("Data Masters");
         Company greyMatter = new Company("Grey Matter");
 
         softwareMachine.getEmployees().add(johnSmith);
@@ -56,6 +60,54 @@ public class CompanyDaoTestSuite {
             companyDao.deleteById(dataMastersId);
             companyDao.deleteById(greyMatterId);
         } catch(Exception e) {
+            //do nothing
+        }
+    }
+
+    @Test
+    void testStartingWithFollowingChars() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company dataMachine = new Company("Data Machine");
+        Company dataMasters = new Company("Data Masters");
+        Company greyMatter = new Company("Grey Matter");
+
+        dataMachine.getEmployees().add(johnSmith);
+        dataMasters.getEmployees().add(stephanieClarckson);
+        dataMasters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(dataMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMasters);
+        lindaKovalsky.getCompanies().add(dataMasters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        companyDao.save(dataMachine);
+        int dataMachineId = dataMachine.getId();
+        companyDao.save(dataMasters);
+        int dataMastersId = dataMasters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        //When
+        List<Company> companyListWithGRE = companyDao.companiesWithNamesStartingWithFollowingCharacters("Gre");
+        List<Company> companyListWithDAT = companyDao.companiesWithNamesStartingWithFollowingCharacters("Dat");
+        List<Company> companyListWithYYY = companyDao.companiesWithNamesStartingWithFollowingCharacters("Yyy");
+        //Then
+        assertEquals(1, companyListWithGRE.size());
+        assertEquals(2, companyListWithDAT.size());
+        assertEquals(0, companyListWithYYY.size());
+        //CleanUp
+        try {
+            companyDao.deleteById(dataMachineId);
+            companyDao.deleteById(dataMastersId);
+            companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
             //do nothing
         }
     }
